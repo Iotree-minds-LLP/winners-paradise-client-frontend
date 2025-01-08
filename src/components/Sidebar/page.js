35119241ae1a5d8ebf88f3f2d8cea6c884b197e6
chinmayIotreeminds <161325682+chinmayIotreeminds@users.noreply.github.com>
2025-01-08 10:37:26 +0530

    enhance PWA popup handling to support device-specific installation instructions for iOS and Android

--- src/components/Sidebar/page.js
+++ src/components/Sidebar/page.js
@@ -69,25 +69,64 @@ const Sidebar = () => {
                     <div class="h-full border border-1 border-gray-100 px-3 py-4 overflow-y-auto bg-white dark:bg-gray-800 ">
                         <ul class="space-y-2 font-medium p-3">
                             {investments === true && (
+                                <>
+                                    <li className="mt-2">
+                                        {currentUrl === "/dashboard" ? (
 
-                                <li className="mt-2">
-                                    {currentUrl === "/dashboard" ? (
+                                            <Link to="/dashboard" class=" text-start flex  items-start p-2 text-white p-4 rounded-lg  dark:text-white bg-gradient-to-l from-[#020065] to-[#0400CB]  group">
+                                                <svg class="flex-shrink-0 w-5 h-5 text-white transition duration-75 dark:text-gray-400 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
+                                                    <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
+                                                </svg>
+                                                <span class="flex-1 ms-3 whitespace-nowrap ">Dashboard</span>
+                                            </Link>
+                                        ) : (
+                                            <Link to="/dashboard" class="text-start flex  items-start p-2 text-gray-900 p-4 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
+                                                    <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
+                                                </svg>
+                                                <span class="flex-1 ms-3 whitespace-nowrap ">Dashboard</span>
+                                            </Link>
+                                        )}
+                                    </li>
 
-                                        <Link to="/dashboard" class=" text-start flex  items-start p-2 text-white p-4 rounded-lg  dark:text-white bg-gradient-to-l from-[#020065] to-[#0400CB]  group">
-                                            <svg class="flex-shrink-0 w-5 h-5 text-white transition duration-75 dark:text-gray-400 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
-                                                <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
-                                            </svg>
-                                            <span class="flex-1 ms-3 whitespace-nowrap ">Dashboard</span>
-                                        </Link>
-                                    ) : (
-                                        <Link to="/dashboard" class="text-start flex  items-start p-2 text-gray-900 p-4 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                            <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
-                                                <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
-                                            </svg>
-                                            <span class="flex-1 ms-3 whitespace-nowrap ">Dashboard</span>
-                                        </Link>
-                                    )}
-                                </li>
+                                    <li>
+                                        {currentUrl === "/payouts" ? (
+                                            <Link
+                                                to="/payouts"
+                                                class="rounded-lg text-start flex items-start p-4 text-white  dark:text-white bg-gradient-to-l from-[#020065] to-[#0400CB] group"
+                                            >
+                                                <svg
+                                                    class="flex-shrink-0 w-6 h-6 text-white transition duration-75 dark:text-gray-400"
+                                                    aria-hidden="true"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="currentColor"
+                                                    viewBox="0 0 20 20"
+                                                >
+                                                    <path d="M10 2a8 8 0 1 0 8 8 8.011 8.011 0 0 0-8-8Zm0 14a6 6 0 1 1 6-6 6.006 6.006 0 0 1-6 6Zm-1-10h2a1 1 0 0 1 1 1v3h2v2h-2v1h-2v-1H7v-2h2V7Zm2 3V8h-2v2h2Z" />
+                                                </svg>
+                                                <span class="flex-1 ms-3 whitespace-nowrap">Payouts Tracker</span>
+                                            </Link>
+                                        ) : (
+                                            <Link
+                                                to="/payouts"
+                                                class="rounded-lg hover:text-black text-start flex items-center p-4 text-gray-900  dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                                            >
+                                                <svg
+                                                    class="text-start flex-shrink-0 w-6 h-6 text-gray-900 dark:text-white"
+                                                    aria-hidden="true"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="currentColor"
+                                                    viewBox="0 0 20 20"
+                                                >
+                                                    <path d="M10 2a8 8 0 1 0 8 8 8.011 8.011 0 0 0-8-8Zm0 14a6 6 0 1 1 6-6 6.006 6.006 0 0 1-6 6Zm-1-10h2a1 1 0 0 1 1 1v3h2v2h-2v1h-2v-1H7v-2h2V7Zm2 3V8h-2v2h2Z" />
+                                                </svg>
+                                                <span class="flex-1 ms-3 whitespace-nowrap">Payouts Tracker</span>
+                                            </Link>
+                                        )}
+                                    </li>
+                                </>
+
+
                             )}
 
                             <li>
@@ -119,41 +158,6 @@ const Sidebar = () => {
                                         </svg>
 
                                         <span class="flex-1 ms-3 whitespace-nowrap">Catalogs</span>
-                                    </Link>
-                                )}
-                            </li>
-                            <li>
-                                {currentUrl === "/payouts" ? (
-                                    <Link
-                                        to="/payouts"
-                                        class="rounded-lg text-start flex items-start p-4 text-white  dark:text-white bg-gradient-to-l from-[#020065] to-[#0400CB] group"
-                                    >
-                                        <svg
-                                            class="flex-shrink-0 w-6 h-6 text-white transition duration-75 dark:text-gray-400"
-                                            aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="currentColor"
-                                            viewBox="0 0 20 20"
-                                        >
-                                            <path d="M10 2a8 8 0 1 0 8 8 8.011 8.011 0 0 0-8-8Zm0 14a6 6 0 1 1 6-6 6.006 6.006 0 0 1-6 6Zm-1-10h2a1 1 0 0 1 1 1v3h2v2h-2v1h-2v-1H7v-2h2V7Zm2 3V8h-2v2h2Z" />
-                                        </svg>
-                                        <span class="flex-1 ms-3 whitespace-nowrap">Payouts Tracker</span>
-                                    </Link>
-                                ) : (
-                                    <Link
-                                        to="/payouts"
-                                        class="rounded-lg hover:text-black text-start flex items-center p-4 text-gray-900  dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-                                    >
-                                        <svg
-                                            class="text-start flex-shrink-0 w-6 h-6 text-gray-900 dark:text-white"
-                                            aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="currentColor"
-                                            viewBox="0 0 20 20"
-                                        >
-                                            <path d="M10 2a8 8 0 1 0 8 8 8.011 8.011 0 0 0-8-8Zm0 14a6 6 0 1 1 6-6 6.006 6.006 0 0 1-6 6Zm-1-10h2a1 1 0 0 1 1 1v3h2v2h-2v1h-2v-1H7v-2h2V7Zm2 3V8h-2v2h2Z" />
-                                        </svg>
-                                        <span class="flex-1 ms-3 whitespace-nowrap">Payouts Tracker</span>
                                     </Link>
                                 )}
                             </li>
